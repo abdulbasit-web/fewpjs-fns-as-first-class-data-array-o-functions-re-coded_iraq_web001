3980b11d789987cf28e6228831c38a974afec78b
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-05-29 19:19:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+function wakeDog(dogName, dogBreed) {
+    console.log(`wake ${dogName} the ${dogBreed}`);
+  }
+  function leashDog(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+  }
+  function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  }
+  function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  }
+  function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  }
+  function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+  }
+
 const routine = [
   function wakeDog(dogName, dogBreed) {
     console.log(`wake ${dogName} the ${dogBreed}`);
@@ -19,6 +38,8 @@ const routine = [
   },
 ];
 
+
+
 function exerciseDog(dogName, dogBreed) {
   let arrayOfRoutine = routine.map((dogRoutineFunc) => {
     return dogRoutineFunc(dogName, dogBreed);
@@ -26,4 +47,5 @@ function exerciseDog(dogName, dogBreed) {
   return arrayOfRoutine;
 }
 
-exerciseDog("Byron", "poodle");
+const arrayOfRoutine = exerciseDog("Bob", "poodle");
+console.log(arrayOfRoutine);
